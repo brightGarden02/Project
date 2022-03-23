1369bfc323452b7b2693f262e505169de46ce571
Juwon Lee <39127771+brightGarden02@users.noreply.github.com>
2022-03-23 17:57:53 +0900

    Update mazeGame.cpp

--- Game/mazeGame.cpp
+++ Game/mazeGame.cpp
@@ -5,28 +5,37 @@ using namespace std;
 
 
 /*
-0:  º®
-1: ±æ
-2: ½ÃÀÛÁ¡
-3: µµÂøÁ¡
+0:  ë²½
+1: ê¸¸
+2: ì‹œìž‘ì 
+3: ë„ì°©ì 
 */
 
 
-struct _tagPoint()
+struct _tagPoint
 {
 	int x;
 	int y;
-}
+};
 
 
-// typedef: Å¸ÀÔÀ» ÀçÁ¤ÀÇÇÏ´Â ±â´ÉÀÌ´Ù. 
+// typedef: íƒ€ìž…ì„ ìž¬ì •ì˜í•˜ëŠ” ê¸°ëŠ¥ì´ë‹¤. 
 typedef _tagPoint POINT;
 typedef _tagPoint* PPOINT;
 
 
 
-void SetMaze(char Maze[21][21])
+void SetMaze(char Maze[21][21], PPOINT pPlayerPos, PPOINT pStartPos, PPOINT pEndPos)
 {
+	pStartPos->x = 0;
+	pStartPos->y = 0;
+
+	pEndPos->x = 19;
+	pEndPos->y = 19;
+
+	*pPlayerPos = *pStartPos;
+
+
 	strcpy_s(Maze[0], "21100000000000000000");
 	strcpy_s(Maze[1], "00111111111100000000");
 	strcpy_s(Maze[2], "00100010100111111100");
@@ -35,46 +44,134 @@ void SetMaze(char Maze[21][21])
 	strcpy_s(Maze[5], "01000000111100111100");
 	strcpy_s(Maze[6], "01000000000000010000");
 	strcpy_s(Maze[7], "01100000000000010000");
-	strcpy_s(Maze[8], "01111111111111111111");
-	strcpy_s(Maze[9], "01100000000000000000");
-	strcpy_s(Maze[10], "01100000000000000000");
-	strcpy_s(Maze[11], "01100000000000000000");
-	strcpy_s(Maze[12], "01100000000000000000");
-	strcpy_s(Maze[13], "01100000000000000000");
-	strcpy_s(Maze[14], "01100000000000000000");
-	strcpy_s(Maze[15], "01100000000000000000");
-	strcpy_s(Maze[16], "01100000000000000000");
-	strcpy_s(Maze[17], "01100000000000000000");
-	strcpy_s(Maze[18], "00100000000000000000");
+	strcpy_s(Maze[8], "01111111111111111110");
+	strcpy_s(Maze[9], "00100000001000000000");
+	strcpy_s(Maze[10], "00100000001000000000");
+	strcpy_s(Maze[11], "00100000111000011110");
+	strcpy_s(Maze[12], "01101000100000010000");
+	strcpy_s(Maze[13], "01001000100000010000");
+	strcpy_s(Maze[14], "01001001100011110000");
+	strcpy_s(Maze[15], "01101111000010010000");
+	strcpy_s(Maze[16], "00100001111110010000");
+	strcpy_s(Maze[17], "01101000000001111100");
+	strcpy_s(Maze[18], "00101001100000000000");
 	strcpy_s(Maze[19], "01111111111111111113");
 
 }
 
-void Output(char Maze[21][21])
+void Output(char Maze[21][21], PPOINT pPlayerPos)
 {
-	for (int i = 0 i < 20; ++i)
+	for (int i = 0; i < 20; ++i)
 	{
 		for (int j = 0; j < 20; ++j)
 		{
-			if (Maze[i][j] == '0')
-				cout << " ";
+
+			if (pPlayerPos->x == j && pPlayerPos->y == i)
+				cout << "â˜†";
+
+			else if (Maze[i][j] == '0')
+				cout << "ã…";
 
 			else if (Maze[i][j] == '1')
 				cout << "  ";
 			else if (Maze[i][j] == '2')
-				cout << "¡Ú";
+				cout << "â˜…";
 
-			else if (Mazr[i][j] == '3')
-				cout << "¡Ý";
+			else if (Maze[i][j] == '3')
+				cout << "â—Ž";
 
 		}
-		cout < endl;
+		cout << endl;
 	}
 }
 
+
+void MoveUp(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->y - 1 >= 0)
+	{
+		// ë²½ì¸ì§€ ì²´í¬í•œë‹¤.
+		if (Maze[pPlayerPos->y - 1][pPlayerPos->x] != '0')
+		{
+			--pPlayerPos->y;
+		}
+	}
+
+}
+
+void MoveDown(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->y + 1 < 20)
+	{
+		// ë²½ì¸ì§€ ì²´í¬í•œë‹¤.
+		if (Maze[pPlayerPos->y + 1][pPlayerPos->x] != '0')
+		{
+			++pPlayerPos->y;
+		}
+	}
+
+}
+
+void MoveLeft(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->x - 1 >= 0)
+	{
+		// ë²½ì¸ì§€ ì²´í¬í•œë‹¤.
+		if (Maze[pPlayerPos->y][pPlayerPos->x - 1] != '0')
+		{
+			--pPlayerPos->x;
+		}
+	}
+
+}
+
+
+void MoveRight(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->x + 1 < 20)
+	{
+		// ë²½ì¸ì§€ ì²´í¬í•œë‹¤.
+		if (Maze[pPlayerPos->y][pPlayerPos->x + 1] != '0')
+		{
+			++pPlayerPos->x;
+		}
+	}
+
+}
+
+
+void MovePlayer(char Maze[21][21], PPOINT pPlayerPos, char cinput)
+{
+
+	switch (cinput)
+	{
+	case 'w':
+	case 'W':
+		MoveUp(Maze, pPlayerPos);
+		break;
+	case 's':
+	case 'S':
+		MoveDown(Maze, pPlayerPos);
+		break;
+	case 'a':
+	case 'A':
+		MoveLeft(Maze, pPlayerPos);
+		break;
+	case 'd':
+	case 'D':
+		MoveRight(Maze, pPlayerPos);
+		break;
+
+	}
+
+}
+
+
+
+
 int main()
 {
-	// 20 * 20 ¹Ì·Î¸¦ ¸¸µç´Ù.
+	// 20 * 20 ë¯¸ë¡œë¥¼ ë§Œë“ ë‹¤.
 	char strMaze[21][21];
 
 	POINT tPlayerPos;
@@ -82,22 +179,31 @@ int main()
 	POINT tEndPos;
 
 
-	// ¹Ì·Î¸¦ ¼³Á¤ÇÑ´Ù. 
-//	SetMaze(strMaze, &tPlayerPos, &tStartPos, &tEndPos);
+	// ë¯¸ë¡œë¥¼ ì„¤ì •í•œë‹¤. 
+	SetMaze(strMaze, &tPlayerPos, &tStartPos, &tEndPos);
 
-	SetMaze(strMaze);
 
 	while (true)
 	{
 		system("cls");
 
-		// ¹Ì·Î¸¦ Ãâ·ÂÇÑ´Ù. 
-		Output(strMaze);
-		cout << "w: À§, s: ¾Æ·¡, a: ¿ÞÂÊ, d: ¿À¸¥ÂÊ, q: Á¾·á:  ";
+		// ë¯¸ë¡œë¥¼ ì¶œë ¥í•œë‹¤. 
+		Output(strMaze, &tPlayerPos);
+		
+		if(tPlayerPos.x == tEndPos.x && tPlayerPos.y == tEndPos.y)
+		{
+			cout << "ë„ì°©í–ˆìŠµë‹ˆë‹¤." << endl;
+			break; 
+		}
+		
+		
+		cout << "w: ìœ„, s: ì•„ëž˜, a: ì™¼ìª½, d: ì˜¤ë¥¸ìª½, q: ì¢…ë£Œ:  ";
 		char cinput = _getch();
 
 		if (cinput == 'q' || cinput == 'Q')
 			break;
+
+		MovePlayer(strMaze, &tPlayerPos, cinput);
 
 	}
 
